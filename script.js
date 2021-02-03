33225c1302f2f44be0b6707640d0d35deef16c33
marcieleadivincula <marcieleadivincula@gmail.com>
2021-02-03 12:53:59 -0300

    Validate data

--- script.js
+++ script.js
@@ -12,35 +12,24 @@ const Modal = {
   },
 };
 
-const btnNewTransition = document
-  .querySelector("#new-transaction")
-  .addEventListener("click", Modal.open);
-const btnCancelTransition = document
-  .querySelector("#cancel-trans")
-  .addEventListener("click", Modal.close);
-
 //TODO PODERIA TRATAR O ERRO para contar os elementos e assim não aceitar valor maior que isso
 const transactions = [
   {
-    id: 1,
     description: "Luz",
     amount: -50001,
     date: "23/01/2021",
   },
   {
-    id: 2,
     description: "WebSite",
     amount: 500000,
     date: "23/01/2021",
   },
   {
-    id: 3,
     description: "Internet",
     amount: -20012,
     date: "23/01/2021",
   },
   {
-    id: 4,
     description: "App",
     amount: 200000,
     date: "23/01/2021",
@@ -51,6 +40,11 @@ const Transaction = {
   all: transactions,
   add(transaction) {
     Transaction.all.push(transaction);
+
+    App.reload();
+  },
+  remove(index) {
+    Transaction.all.splice(index, 1);
 
     App.reload();
   },
@@ -123,9 +117,23 @@ const DOM = {
       Transaction.total()
     );
   },
+  clearTransactions() {
+    DOM.transactionsContainer.innerHTML = "";
+  },
 };
 
 const Utils = {
+  formatAmount(value) {
+    //value = Number(value.replace(/\,\./g, "")) * 100; //remove vírgula e ponto, depois multiplica por 100
+    value = Number(value) * 100;
+
+    return value;
+  },
+  formatDate(date) {
+    const splittedDate = date.split("-");
+
+    return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
+  },
   formatCurrency(value) {
     const signal = Number(value) < 0 ? "-" : "";
 
@@ -142,6 +150,75 @@ const Utils = {
   },
 };
 
+const Form = {
+  description: document.querySelector("input#description"),
+  amount: document.querySelector("input#amount"),
+  date: document.querySelector("input#date"),
+
+  getValues() {
+    return {
+      description: Form.description.value,
+      amount: Form.amount.value,
+      date: Form.date.value,
+    };
+  },
+
+  formatValues() {
+    let { description, amount, date } = Form.getValues();
+
+    amount = Utils.formatAmount(amount);
+
+    date = Utils.formatDate(date);
+
+    return {
+      description,
+      amount,
+      date,
+    };
+  },
+  validateFields() {
+    // const description = Form.getValues().description;
+    const { description, amount, date } = Form.getValues();
+    if (
+      description.trim() === "" ||
+      amount.trim() === "" ||
+      date.trim() === ""
+    ) {
+      throw new Error("Por favor, preencha todos os campos!");
+    }
+  },
+  // saveTransaction(transaction) {
+  //   Transaction.add(transaction);
+  // },
+  clearFields() {
+    Form.description.value = "";
+    Form.amount.value = "";
+    Form.date.value = "";
+  },
+  submit(event) {
+    event.preventDefault();
+
+    try {
+      //verificar se todas as informações foram preenchidas
+      Form.validateFields();
+
+      //Formatar os dados para Salvar
+      const transaction = Form.formatValues();
+
+      //Salvar
+      Transaction.add(transaction);
+
+      //Limpar os campos do formulário
+      Form.clearFields();
+
+      //Modal feche
+      Modal.close();
+    } catch (error) {
+      alert(error.message);
+    }
+  },
+};
+
 const App = {
   init() {
     Transaction.all.forEach((transaction) => DOM.addTransaction(transaction));
@@ -149,15 +226,16 @@ const App = {
     DOM.updateBalance();
   },
   reload() {
+    DOM.clearTransactions();
     App.init();
   },
 };
 
 App.init();
 
-Transaction.add({
-  id: 39,
-  description: "Olá",
-  amount: 200,
-  date: "23/01/2021",
-});
+const btnNewTransition = document
+  .querySelector("#new-transaction")
+  .addEventListener("click", Modal.open);
+const btnCancelTransition = document
+  .querySelector("#cancel-trans")
+  .addEventListener("click", Modal.close);
